3c671698492e7f3ec635e2d112032b8cb87e8643
Gregory <garthus@adminux.fr>
2021-04-03 13:41:58 +0200

    debug(): not a bug

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,21 @@
 # ssh client configuration
+file [ '/home/ubuntu/.ssh':
+      ensure => directory,
+}
+file { '/home/ubuntu/.ssh/ssh_config':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+}
 file_line { 'Declare identity file':
       ensure => present,
-      path   => '/etc/ssh/ssh_config',
+      path   => '/home/ubuntu/.ssh/ssh_config',
       line   => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'Turn off passwd auth':
       ensure => present,
-      path   => '/etc/ssh/ssh_config',
+      path   => '/home/ubuntu/.ssh/ssh_config',
       line   => 'PasswordAuthentication no',
 }
