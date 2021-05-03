774e5bc16afa4a95b79986e26f22bfe336d5de6e
Gregory <garthus@adminux.fr>
2021-05-03 11:32:47 +0200

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" module requests and sys """
+import requests
+import sys
+import csv
+import json
+
+
+if (len(sys.argv) == 2):
+
+    file = "{}.csv".format(sys.argv[1])
+    data_file = open(file, 'w')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    r1 = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    for line in r.json():
+        if line["userId"] == int(sys.argv[1]):
+            data = [line['userId'],
+                    r1.json()[int(sys.argv[1])]['name'],
+                    line['completed'],
+                    line['title']]
+            writer = csv.writer(data_file,
+                                quoting=csv.QUOTE_ALL,
+                                delimiter=',')
+            writer.writerow(data)
